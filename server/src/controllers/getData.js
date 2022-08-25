2e089fd469a58860e543d17ef87f386d123c0349
Karan Kumar <krndhamija9@gmail.com>
2022-08-26 02:32:22 +0530

    Delete server directory

--- server/src/controllers/getData.js
+++
@@ -1,6 +0,0 @@
-const videoModel = require("../models/videoModel")
-
-const getData = async function (req, res) {
-
-}
-module.exports = { getData }
--- server/src/routes/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getData } = require('../controllers/getData');
-const router = express.Router();
-
-router.get("/", getData);
-
-module.exports = router;
